d8f68faa10aa0c99ef5bd33f7bf518cd0ba2c718
Ernie Yang <ernieyang09@hotmail.com>
2019-09-08 19:38:25 +0800

    Add frontend query function

--- client/src/pages/View/View.tsx
+++ client/src/pages/View/View.tsx
@@ -1,6 +1,18 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
+import { Subject } from 'rxjs';
+
+import {
+  debounceTime,
+  tap,
+  switchMap,
+  map,
+} from 'rxjs/operators';
+
+import { ajax } from 'rxjs/ajax';
+
 import {
   SearchInput,
   Pane,
@@ -17,10 +29,23 @@ import {
   loadTodos,
 } from '@modules/todo';
 
+import { ITodo } from '@modules/type';
+
+
+const search$ = new Subject();
+
+
+
 const View = () => {
 
   const [isLoading, setIsLoading] = useState(true);
+  const [search, setSearch] = useState('');
   const dispatch = useDispatch();
+
+  const onSearchChange = (e) => {
+    setSearch(e.target.value);
+    search$.next(e.target.value);
+  }
 
   useEffect(()=> {
     fetch('/api/todo').then(
@@ -29,12 +54,41 @@ const View = () => {
       dispatch(loadTodos(todos));
       setIsLoading(false);
     })
-  })
+  }, [])
+
+  useEffect(() => {
+    const obs$ = search$.pipe(
+      debounceTime(500),
+      tap(() => { setIsLoading(true) }),
+      switchMap(x =>
+        ajax({
+          url: `/api/todo${x && `?q=${x}`}`,
+          method: 'GET',
+        })
+        .pipe(
+          map(ajax => ajax.response)
+        )
+      )
+    ).subscribe(
+      (todos: ITodo[]) => {
+        dispatch(loadTodos(todos));
+        setIsLoading(false);
+      }
+    );
+
+    return () => {
+      obs$.unsubscribe();
+    }
+  }, []);
 
   const todos = useSelector(TodoSelector);
   return (
     <Page>
-      <SearchInput width="100%" />
+      <SearchInput
+        width="100%"
+        onChange={onSearchChange}
+        value={search}
+      />
       {
         isLoading ?
           <Pane>
